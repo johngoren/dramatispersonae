eb8baa5dfe3cc95888cb0548db71f1c393f74520
John Gorenfeld <john@gorenfeld.co.uk>
2022-02-10 15:05:46 +0000

    ...

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import controllers
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>TODO start NLP</p>"
+    return "<p>TODO send welcome form template</p>"
+
+@app.route("/query")
+def post():
+    return "<p>TODO set up POST that fires up NLP</p>"
