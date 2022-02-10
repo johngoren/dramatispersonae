4b6dd993227bca06b4b0d4b7c0e4cd1cdf32a6bc
John Gorenfeld <john@gorenfeld.co.uk>
2022-02-10 10:59:43 +0000

    Parsing a classic Jimmy Breslin piece

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ def get_example():
         data = file.read()
         return data
 
+def is_person(entry):
+    type = entry[1]
+    if type == "PERSON":
+        return True
+    return False
+
 example = get_example()
-doc = nlp(example)
+nlp_entries = nlp(example)
+tuples = [(X.text, X.label_) for X in nlp_entries.ents]
+just_people = filter(is_person, tuples)
 
-print([(X.text, X.label_) for X in doc.ents])
+print(list(just_people))
 
+# print([(X.text, X.label_) for X in nlp_entries.ents])
+
+# filtered_for_people = filter(is_person, nlp_entries)
+# people = list(filtered_for_people)
+# print(people)
 
