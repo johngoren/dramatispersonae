ab6bb548457dabfcb53f7b11aa4d279414b320a8
John Gorenfeld <john@gorenfeld.co.uk>
2022-02-10 11:07:51 +0000

    let's merge people who are the same but mentioned differently

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Experiments in the ML technique of NEP (named entity recognition), used on journalism pieces. 
 
 Ever been halfway down a long feature article, seen a last name, and asked yourself, "Who is that again?" and had to scroll back up? This will be a browser plugin to solve that problem. It will build lists of first identifications of people in stories, and then let you refer to them while reading.
+
+Testing this on the classic 1980 Jimmy Breslin piece, "Are You John Lennon?" from the New York Daily News.
--- main.py
+++ main.py
@@ -15,12 +15,23 @@ def is_person(entry):
         return True
     return False
 
+def get_first_mention(name):
+    # TODO: Extract first sentence mentioning the person, and add it to our dramatis personae.
+    # TODO: More advanced ML version will turn the sentence into a capsule bio.
+    pass
+
+def merge_equivalents(names):
+    # TODO: Merge items that contain other items, like last names duplicated by full names.
+    return names
+
 example = get_example()
 nlp_entries = nlp(example)
 tuples = [(X.text, X.label_) for X in nlp_entries.ents]
-just_people = filter(is_person, tuples)
+just_people = set(filter(is_person, tuples))
+merged_equivalents = merge_equivalents(just_people)
 
-print(set(just_people))
+# TODO: 
+print(set(merged_equivalents))
 
 # print([(X.text, X.label_) for X in nlp_entries.ents])
 
