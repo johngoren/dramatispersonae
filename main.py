a2d62ea21551b5caf3dda4b4a83568d797ce63f7
John Gorenfeld <john@gorenfeld.co.uk>
2022-02-10 11:20:04 +0000

    removing dupes

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from spacy import displacy
 from collections import Counter
 nlp = spacy.load("en_core_web_sm")
 
+__LABEL__ = 0
+__TYPE__ = 1
 
 def get_example():
     with open('./assets/breslin.txt', 'r') as file:
@@ -10,10 +12,24 @@ def get_example():
         return data
 
 def is_person(entry):
-    type = entry[1]
+    type = entry[__TYPE__]
     if type == "PERSON":
         return True
     return False
+
+def is_mentioned_elsewhere_in_longer_form(entries, name):
+    for entry in entries:
+        label = entry[__LABEL__]
+        if label_is_same_name_in_longer_form(label, name):
+            return True
+    return False
+
+def label_is_same_name_in_longer_form(label, name):
+    if label is not name:
+        if name in label:
+            return True
+    return False
+
 
 def get_first_mention(name):
     # TODO: Extract first sentence mentioning the person, and add it to our dramatis personae.
@@ -22,16 +38,16 @@ def get_first_mention(name):
 
 def merge_equivalents(names):
     # TODO: Merge items that contain other items, like last names duplicated by full names.
-    return names
+    return set(filter(is_mentioned_elsewhere_in_longer_form, names))    
 
 example = get_example()
 nlp_entries = nlp(example)
 tuples = [(X.text, X.label_) for X in nlp_entries.ents]
 just_people = set(filter(is_person, tuples))
-merged_equivalents = merge_equivalents(just_people)
+merged_equivalents = [entry for entry in just_people if is_mentioned_elsewhere_in_longer_form(entry, just_people) is False]
 
 # TODO: 
-print(set(merged_equivalents))
+print(merged_equivalents)
 
 # print([(X.text, X.label_) for X in nlp_entries.ents])
 
